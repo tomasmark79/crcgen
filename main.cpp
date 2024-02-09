822687c4a46ba1c2996f67cdb91d492131de5b62
tomasmark79 <tomas@digitalspace.name>
2024-02-09 21:07:45 +0100

    clean unnecessary includes

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 //MIT License
 //
-//Copyright (c) 2024 Tomáš Mark
+//Copyright (c) 2024 Tomas Mark
 //
 //Permission is hereby granted, free of charge, to any person obtaining a copy
 //of this software and associated documentation files (the "Software"), to deal
@@ -21,7 +21,7 @@
 //SOFTWARE.
 
 
-#include <iostream>
+
 #include "Options.h"
 
 using namespace std;
